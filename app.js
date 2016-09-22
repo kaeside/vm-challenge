a5ac0e81f2ae45f3a06d3a656d31c1c7bbea3fab
kaeside <kaeside.iwuagwu@gmail.com>
2016-09-22 19:35:08 -0400

    WIP draggable items

--- app.js
+++ app.js
@@ -1,75 +1,84 @@
 var api = 'http://jsonplaceholder.typicode.com';
-//call for users
-$.ajax({
-    url: api + '/users/1',
-    method: 'GET'
-}).then(function(data) {
-    $('.user').text(data.name);
-});
 
-$.ajax({
-    url: api + '/users/2',
-    method: 'GET'
-}).then(function(data) {
-    $('.user').text(data.name);
-});
+var userName;
+var albumTitle;
+var albumId;
 
-//calls for albums
-$.ajax({
-    url: api + '/albums/1',
-    method: 'GET'
-}).then(function(data) {
-    $('.album-id').text(data.title);
-    $('.album-title').text(data.id);
-});
+function getAllUsers() {
+    $.get(api + '/users/', function(data) {
+        $('.user-one').text(data[0].name);
+        $('.user-two').text(data[1].name);
+    });
+}
 
-$.ajax({
-    url: api + '/albums/2',
-    method: 'GET'
-}).then(function(data) {
-    $('.album-id').text(data.title);
-    $('.album-title').text(data.id);
-});
+// function getUser(id) {
+//     $.get(api + '/users/' + id, function(data) {
+//         console.log(data.name);
+//         $('.user').text(data.name);
+//     });
+// }
+
+function getAllAlbums() {
+    $.get(api + '/albums/', function(data) {
+        $('.album-title-one').text(data[0].title);
+        $('.album-id-one').text(data[0].id);
+        $('.album-title-two').text(data[1].title);
+        $('.album-id-two').text(data[1].id);
+    });
+}
+
+// function getAlbum(id) {
+//     $.get(api + '/albums/' + id, function(data) {
+//         console.log(data.title);
+//         $('.album-id').text(data.title);
+//         $('.album-title').text(data.id);
+//     });
+// }
+
+getAllUsers();
+getAllAlbums();
+
+function newTable() {
+
+}
+// getUser(1);
+// getAlbum(1);
+
 
 //DRAGGING FUNCTIONS
 
-function handleDragEnter() {
+function handleDragEnter(e) {
+    e.preventDefault();
 
 }
 
-function handleDragOver() {
 
+function handleDragLeave(e) {
+    e.preventDefault();
+    // ("Drag the album and album ID here!");
+    return false;
 }
 
-function handleDragLeave() {
 
+function handleDragStart(e) {
+    e.originalEvent.preventDefault();
+    var data = e.originalEvent.dataTransfer.setData("text", e.target.id);
 }
 
-function handleDragOver() {
-
+function handleDragOver(e) {
+    e.preventDefault();
 }
 
-// function handleDragStart(e) {
-//     this.style.color = 'blue';
-// }
-
-var dropTarget = document.getElementsByClassName('drop-target');
-
-dropTarget.addEventListener('dragenter',handleDragEnter);
-dropTarget.addEventListener('dragOver',handleDragOver);
-dropTarget.addEventListener('dragleave',handleDragLeave);
-dropTarget.addEventListener('drop',handleDrop);
+function handleDrop(e) {
+    e.preventDefault();
+    console.log('SUCCSESSFUL DROP!');
+    var data = e.originalEvent.dataTransfer.getData("text");
+    e.originalEvent.target.appendChild(document.getElementById(data));
+}
 
 
-
-
-
-
-
-/* <div class="table">
-    <p class="user"></p>
-    <ul>
-        <li class="album-title"></li>
-        <li class="album-id"></li>
-    </ul>
-</div> */
+$('.drop-target').on('dragenter',handleDragEnter);
+$('#drag-target').on('dragover',handleDragOver);
+$('.drop-target').on('dragleave',handleDragLeave);
+$('#drag-target').on('dragstart',handleDragStart);
+$('#drag-target').on('drop',handleDrop);
